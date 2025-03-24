bcd10e1d31d7666d32e48cf1b27277924cf1fd3c
Benh-L <benhui.lee@gmail.com>
2025-03-24 11:38:46 +0000

    done

--- Lab5.py
+++ Lab5.py
@@ -5,18 +5,44 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    row = 0
+    square = ''
+    while row < n:
+        if row == 0 or row == n - 1:
+            square += '*' * n
+        else:
+            square +='*' + ' ' * (n - 2) + '*'
+        if row < n - 1:
+            square += '\n'
+        row += 1
+    return square
+
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    row = 1
+    number_pattern = ''
+    while row <= n:
+        col = 1
+        while col <= row:
+            number_pattern += str(col)
+            col += 1
+        if row < n:
+            number_pattern += '\n'
+        row += 1
+    return number_pattern
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+    total = 0
+    count = 1
+    while count <= n:
+        total += count
+        count += 1
+    return total
 
 # Example for n = 4:
 #    *
@@ -24,4 +50,9 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    row = 0
+    pyramid = ''
+    while row < n:
+        pyramid += ' ' * (n - row - 1) + '*' * (2 * row + 1) + '\n'
+        row += 1
+    return pyramid.rstrip()
